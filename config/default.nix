f8eb71fea7333c0c383113575c62811e95e222b3
LilleAila <olai.solsvik@gmail.com>
2024-03-23 18:44:51 +0100

    Initial commit

---
+++ config/default.nix
@@ -0,0 +1,22 @@
+{
+  imports = [
+  ];
+
+	colorschemes.gruvbox.enable = true;
+
+	plugins.lualine.enable = true;
+	plugins.bufferline.enable = true;
+
+	plugins.lsp = {
+		enable = true;
+		servers = {
+			tsserver.enable = true;
+			lua-ls.enable = true;
+		};
+	};
+
+	plugins.telescope.enable = true;
+	plugins.oil.enable = true;
+	plugins.treesitter.enable = true;
+	plugins.luasnip.enable = true;
+}
