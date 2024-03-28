619c32a719e070db2bdd0fc3e1696acf920640eb
LilleAila <olai.solsvik@gmail.com>
2024-03-28 10:15:54 +0100

    Changed back

--- flake.nix
+++ flake.nix
@@ -19,7 +19,7 @@
     flake-parts,
     ...
   } @ inputs:
-    inputs.flake-parts.lib.mkFlake {inherit inputs;} {
+    flake-parts.lib.mkFlake {inherit inputs;} {
       systems = [
         "x86_64-linux"
         "aarch64-linux"
