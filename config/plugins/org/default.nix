17788cf530b4091b71b47f7a7d1bce73f88bffcc
LilleAila <olai.solsvik@gmail.com>
2024-04-02 17:25:30 +0200

    Added orgmode capture templates

--- config/plugins/org/default.nix
+++ config/plugins/org/default.nix
@@ -2,6 +2,7 @@
   pkgs,
   inputs,
   colorScheme,
+  mkKeymap,
   ...
 }: {
   # This does not work for some reason.. probably the luarocks dependency stuff mentioned in the readme.
@@ -37,8 +38,10 @@
     lua
     */
     ''
+      vim.cmd("language en_US.utf8")
       require("orgmode").setup({
-        org_agenda_files = "~/org/agenda/*",
+        org_agenda_files = { "~/org/*", "./notes.org" }, -- Add both global and project-specific notes to the agenda
+        -- Remember to refresh agenda with `r` when changing directories.
         org_default_notes_file = "~/org/notes.org",
         org_startup_folded = "overview",
 
@@ -49,11 +52,50 @@
           NEXT = ":foreground #${c.base00} :background #${c.base09} :weight bold :slant italic",
           DONE = ":foreground #${c.base00} :background #${c.base0B}",
         },
+        org_capture_templates = {
+          -- Global
+          T = {
+            description = "Task",
+            template = "* TODO %?",
+            datetree = { reversed = true, tree_type = "day" },
+          },
+          N = {
+            description = "Note",
+            template = "* %?",
+            datetree = { reversed = true, tree_type = "day" },
+          },
+
+          -- Project-specific
+          t = {
+            description = "Local task",
+            template = "* TODO %?",
+            datetree = { reversed = true, tree_type = "day" },
+            target = "./notes.org",
+          },
+          n = {
+            description = "Local note",
+            template = "* %?",
+            datetree = { reversed = true, tree_type = "day" },
+            target = "./notes.org",
+          },
+          P = {
+            description = "Local project task",
+            template = "* TODO %?\n %U\n %a\n",
+            datetree = { reversed = true, tree_type = "day" },
+            target = "./notes.org",
+          },
+          p = {
+            description = "Local project note",
+            template = "* %?\n %U\n %a\n",
+            datetree = { reversed = true, tree_type = "day" },
+            target = "./notes.org",
+          },
+        },
 
         org_archive_location = "%s_archive::",
         org_hide_leading_stars = false,
         org_hide_emphasis_markers = false,
-        org_ellipsis = "...", -- Shown when headline is folded
+        org_ellipsis = "...", -- Text shown when headline is folded
         org_log_done = "time",
         org_log_into_drawer = nil,
         org_highlight_latex_and_related = "entities",
@@ -80,4 +122,9 @@
   plugins.which-key.registrations = {
     "<leader>o".name = " Org";
   };
+
+  keymaps = [
+    (mkKeymap "n" "<leader>on" ":e ./notes.org<cr>" "Edit local notes")
+    (mkKeymap "n" "<leader>oN" ":e ~/org/notes.org<cr>" "Edit global notes")
+  ];
 }
