e288a8c0e59d10dc6a7a8ae187388d379a16a1c9
LilleAila <olai.solsvik@gmail.com>
2024-06-04 15:57:36 +0200

    oops forgot to correct path and also infinite recursion

--- config/plugins/ui/colorscheme.nix
+++ config/plugins/ui/colorscheme.nix
@@ -5,7 +5,7 @@
 }: {
   extraPlugins = [
     {
-      plugin = import ./colorscheme.nix {inherit pkgs;} {scheme = colorScheme;};
+      plugin = import ../colorscheme.nix {inherit pkgs;} {scheme = colorScheme;};
       config = "colorscheme nix-${colorScheme.slug}";
     }
   ];
