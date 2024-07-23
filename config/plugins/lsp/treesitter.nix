a06e0ddabccd1c42676b7c2b616454b731e4a544
LilleAila <olai.solsvik@gmail.com>
2024-07-23 09:43:57 +0200

    enabled ts highlight

--- config/plugins/lsp/treesitter.nix
+++ config/plugins/lsp/treesitter.nix
@@ -3,6 +3,7 @@
     enable = true;
     nixvimInjections = true;
     settings.indent.enable = false; # Breaks orgmode or something
+    settings.highlight.enable = true;
     settings.highlight.disable = [
       "tex"
       "latex"
