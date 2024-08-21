64d1d2d0996a151cbdf9f2b947cfde3af9b6f322
LilleAila <olai.solsvik@gmail.com>
2024-08-21 09:11:12 +0200

    add most of the math snippets

--- config/plugins/utils/luasnip/snippets/tex/math.lua
+++ config/plugins/utils/luasnip/snippets/tex/math.lua
@@ -30,11 +30,13 @@ local autosnippet = ls.extend_decorator.apply(s, { snippetType = "autosnippet" }
 local tex = require("luasnip_helpers.latex.conditions")
 local scaffolding = require("luasnip_helpers.latex.scaffolding")
 
-local snip = ls.extend_decorator.apply(parse, {
+-- math snippet
+local msnip = ls.extend_decorator.apply(parse, {
   snippetType = "autosnippet",
   condition = tex.in_math,
   show_condition = tex.in_math
 })
+-- text snippet
 local tsnip = ls.extend_decorator.apply(parse, {
   snippetType = "autosnippet",
   condition = tex.in_text,
@@ -42,8 +44,36 @@ local tsnip = ls.extend_decorator.apply(parse, {
 })
 
 M = {
-  tsnip({ trig = "mm", name = "inline math" }, [[$ $1 $]])
-  -- TODO: resten av snipetene
+  -- init math
+  tsnip({ trig = "mm", name = "inline math" }, [[$ $1 $]]),
+  tsnip({ trig = "mf", name = "flalign math" }, [[
+    \begin{flalign*}
+      $1&$2 & $0
+    \end{flalign*}
+  ]]),
+  tsnip({ trig = "ma", name = "align math" }, [[
+    \begin{align*}
+      $1&$2 & $0
+    \end{align*}
+  ]]),
+  -- align-environments
+  -- for some reason \\ expands to \ even when it's inside [[]] /shrug
+  msnip({ trig = "nn", name = "newline" }, [[
+    \\\\
+    $1&$2 & $0
+  ]]),
+  msnip({ trig = "na", name = "newline with answer" }, [[
+    \\\\
+    \ans{$1&$2} & $0
+  ]]),
+  msnip({ trig = "^", name = "exponent", wordTrig = false }, [[^{$1}]]),
+  msnip({ trig = "*", name = "multiplication" }, [[*]]),
+  -- TODO: skrive disse om til å bruke get_visual? det er potensielt mulig med extend_decorator, eller i hvertfall en mellomfunksjon
+  msnip({ trig = "und", name = "underset" }, [[\underset{$1}{$2}]]),
+  msnip({ trig = "aa", name = "answer" }, [[\ans{$1}]]),
+  msnip({ trig = "sr", name = "square root" }, [[\sqrt{$1}]]),
+  msnip({ trig = "sr", name = "nth root", [[\sqrt[$1]{$2}]]}),
+  -- TODO: de list-relaterte snippetene i https://github.com/LilleAila/nvim-nix/blob/ff3c6efdd0ddc178a9dab31747d7cfbc76f970b8/config/plugins/utils/luasnip/snippets/tex/math.lua (commiten før jeg startet med dette)
 }
 
 return M
