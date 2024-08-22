5d3eb60b57e8a60f54def1a785c4aa282895a80a
LilleAila <olai.solsvik@gmail.com>
2024-08-22 18:57:09 +0200

    spellchecking: finished config

--- config/plugins/lsp/spellcheck.nix
+++ config/plugins/lsp/spellcheck.nix
@@ -1,27 +1,44 @@
-# TODO: read readmes, add keybinds, specify default languages (from readme), add toggle for nynorsk / bokmål?
-# Using FTP and HTTPS as the protocol both result in the same file, so i don't know which is faster to use
 { pkgs, mkKeymap, ... }:
+let
+  # `globpath(&rtp, "spell/*.spell")` from `spell/cleanadd.vim` expects there to be a subdir called `spell` in the runtimepath.
+  # This means that i have to use a subdir of a dir in the runtimepath, thus `.spell/spell`
+  spellDir = "$HOME/.spell";
+  mkSpellFile = lang: "${spellDir}/spell/${lang}.utf-8.add";
+  global_lang = "en_us";
+  global_file = mkSpellFile "en_us";
+in
 {
-  # Dir has to exist for this to work. should be synced with syncthing
+  # spellDir/spell has to exist for this to work, otherwise an error is thrown. should be synced with syncthing
   opts = {
-    spell = true;
-    spl = "en_us";
-    spf = "$HOME/.spell/en_us.utf-8.add";
+    spell = false; # Idk if it should be on or off by default, but it can be slow in big files
+    # TODO: either create an autocommand checking the file size, check if cwd is obsidian or other notes or something similar
+    spl = global_lang;
+    spf = global_file;
   };
 
+  # Otherwise the cleanup script complains about not finding the path through a very cryptic error `No such variable: "s:fname"`, which just means that it has to be on runtimepath
+  extraConfigLua = ''
+    vim.opt.runtimepath:append("${spellDir}")
+  '';
+
   plugins.which-key.registrations = {
+    # p because sPell-check
     "<leader>p".name = " Spellcheck";
     "<leader>pl".name = "󰗊 Language";
+    "<leader>pL".name = "󰗊 Buffer language";
   };
 
   keymaps =
     let
       # :h spellfile, :h spelllang
-      mkSpell = lang: ":set spl=${lang},en_us spf=$HOME/.spell/${lang}.utf-8.add<cr>";
+      # The spellfile for the global lang is always enabled
+      # Whichever file comes first in `spellfile`or `spf` is the one that gets written to
+      _mkSpell = lang: "spl=${lang},${global_lang} spf=${mkSpellFile lang},${global_file}<cr>";
+      mkSpell = lang: ":set ${_mkSpell lang}";
+      mkSpell' = lang: ":setlocal ${_mkSpell lang}";
     in
     [
-      # Switch between langs. Always have english as secondary. Could probably declare in a better way.
-      # The first language of `spelllang` (or `spl`) is used for the personal spell file
+      # Switch between langs. Always have english as secondary.
       (mkKeymap "n" "<leader>plb" (mkSpell "nb") "Norsk bokmål")
       (mkKeymap "n" "<leader>pln" (mkSpell "nn") "Norsk nynorsk")
       (mkKeymap "n" "<leader>ple" (mkSpell "en_us") "US english")
@@ -29,77 +46,66 @@
       (mkKeymap "n" "<leader>plt" (mkSpell "de") "Deutsch")
       (mkKeymap "n" "<leader>pld" (mkSpell "da") "Dansk")
       (mkKeymap "n" "<leader>pls" (mkSpell "sv") "Svenska")
+      # Switch between langs in local buffer
+      (mkKeymap "n" "<leader>pLb" (mkSpell' "nb") "Norsk bokmål")
+      (mkKeymap "n" "<leader>pLn" (mkSpell' "nn") "Norsk nynorsk")
+      (mkKeymap "n" "<leader>pLe" (mkSpell' "en_us") "US english")
+      (mkKeymap "n" "<leader>pLf" (mkSpell' "fr") "Français")
+      (mkKeymap "n" "<leader>pLt" (mkSpell' "de") "Deutsch")
+      (mkKeymap "n" "<leader>pLd" (mkSpell' "da") "Dansk")
+      (mkKeymap "n" "<leader>pLs" (mkSpell' "sv") "Svenska")
+      # Other spell-related keymaps (these are actually longer to type than just using the z prefix)
+      # but i prefer having them all under <leader>, and it makes them easier to remember
+      (mkKeymap "n" "<leader>pg" "zg" "Mark as correct")
+      (mkKeymap "n" "<leader>pw" "zw" "Mark as wrong")
+      (mkKeymap "n" "<leader>pr" "zr" "Mark as rare") # Correct but rarely used
+      (mkKeymap "n" "<leader>pu" "zuw" "Undo mark")
+      (mkKeymap "n" "<leader>pp" ":WhichKey z=<cr>" "See suggestions")
+      (mkKeymap "n" "<leader>pn" "]s" "Next misspelled word")
+      (mkKeymap "n" "<leader>pN" "]s" "Previous misspelled word")
+      (mkKeymap "n" "<leader>pc" ":runtime spell/cleanadd.vim | noh<cr>" "Clean spell file")
+      (mkKeymap "n" "<leader>ps" ":mkspell! %<cr>" "Save .add file")
+      (mkKeymap "n" "<leader>pt" ":setlocal spell!<cr>" "Toggle spellcheck")
     ];
 
   # I probably don't actually need any other files than the utf-8 versions
   # utf-8 is hard-coded in `spellfile` anyways, so i think it'd throw an error with a non-utf-8 file
+  # Can also use https:// - both result in the same file
   extraFiles = {
     # https://ftp.nluug.nl/pub/vim/runtime/spell/README_nb.txt
     "spell/nb.utf-8.spl".source = pkgs.fetchurl {
       url = "ftp://ftp.nluug.nl/pub/vim/runtime/spell/nb.utf-8.spl";
       hash = "sha256-PImo8hi6Wk9Lt+OO6O+yFezcE2/R4lFTIihEZ7JD/88=";
     };
-    "spell/nb.latin1.spl".source = pkgs.fetchurl {
-      url = "ftp://ftp.nluug.nl/pub/vim/runtime/spell/nb.latin1.spl";
-      hash = "sha256-S8yq3lr9gm6CFBHVmzHwgctV0Y5ll7ySGkoERgz7ipg=";
-    };
     # https://ftp.nluug.nl/pub/vim/runtime/spell/README_nn.txt
     "spell/nn.utf-8.spl".source = pkgs.fetchurl {
       url = "ftp://ftp.nluug.nl/pub/vim/runtime/spell/nn.utf-8.spl";
       hash = "sha256-PSnDZyF/Qur3wJHCBSr0hMWPimxa7dRjq+RDo86kS8A=";
-    };
-    "spell/nn.latin1.spl".source = pkgs.fetchurl {
-      url = "ftp://ftp.nluug.nl/pub/vim/runtime/spell/nn.latin1.spl";
-      hash = "sha256-NV3pIZ0yoDBc5b70z/ozG8rwFS7rmeEJsb5i9AwjV7c=";
     };
     # https://ftp.nluug.nl/pub/vim/runtime/spell/README_fr.txt
     "spell/fr.utf-8.spl".source = pkgs.fetchurl {
       url = "ftp://ftp.nluug.nl/pub/vim/runtime/spell/fr.utf-8.spl";
       hash = "sha256-q/uXArmNiHwXWs5Y8as5cz3AjQO2dNkU9WNE74bmO2E=";
     };
-    "spell/fr.latin1.spl".source = pkgs.fetchurl {
-      url = "ftp://ftp.nluug.nl/pub/vim/runtime/spell/fr.latin1.spl";
-      hash = "sha256-CGzNoIkVlMk+qxQ6qD/7vSXQE8G4KGa7tIuxy3iMwv8=";
-    };
     # https://ftp.nluug.nl/pub/vim/runtime/spell/README_en.txt
     "spell/en.utf-8.spl".source = pkgs.fetchurl {
       url = "ftp://ftp.nluug.nl/pub/vim/runtime/spell/en.utf-8.spl";
       hash = "sha256-/sq9yUm2o50ywImfolReqyXmPy7QozxK0VEUJjhNMHA=";
-    };
-    "spell/en.latin1.spl".source = pkgs.fetchurl {
-      url = "ftp://ftp.nluug.nl/pub/vim/runtime/spell/en.latin1.spl";
-      hash = "sha256-Yg2e/Nec/J1jmBj7UoB+Pa5ho3yADWlKAQzVJaIWGEU=";
-    };
-    "spell/en.ascii.spl".source = pkgs.fetchurl {
-      url = "ftp://ftp.nluug.nl/pub/vim/runtime/spell/en.ascii.spl";
-      hash = "sha256-zry6SJ1F2jNVlA80BYLiDONezc1E+cwWi+hz8I54JEk=";
     };
     # https://ftp.nluug.nl/pub/vim/runtime/spell/README_de.txt
     "spell/de.utf-8.spl".source = pkgs.fetchurl {
       url = "ftp://ftp.nluug.nl/pub/vim/runtime/spell/de.utf-8.spl";
       hash = "sha256-c8cQfqM5hWzb6SHeuSpFk5xN5uucByYdobndGfaDo9E=";
     };
-    "spell/de.latin1.spl".source = pkgs.fetchurl {
-      url = "ftp://ftp.nluug.nl/pub/vim/runtime/spell/de.latin1.spl";
-      hash = "sha256-iedU+cMNolKlNP6aSzTeDV/72DlnuqWccb/yb/UAw0I=";
-    };
     # https://ftp.nluug.nl/pub/vim/runtime/spell/README_da.txt
     "spell/da.utf-8.spl".source = pkgs.fetchurl {
       url = "ftp://ftp.nluug.nl/pub/vim/runtime/spell/da.utf-8.spl";
       hash = "sha256-kmorKLt1tRESopA8mLD1HfFvEMuDcvXugpH4Y2nAiTI=";
     };
-    "spell/da.latin1.spl".source = pkgs.fetchurl {
-      url = "ftp://ftp.nluug.nl/pub/vim/runtime/spell/da.latin1.spl";
-      hash = "sha256-HFpTgE2YQpYbIGHQvnLSQ4cdTvqnGZ76SbiN6d9iFMg=";
-    };
     # https://ftp.nluug.nl/pub/vim/runtime/spell/README_sv.txt
     "spell/sv.utf-8.spl".source = pkgs.fetchurl {
       url = "ftp://ftp.nluug.nl/pub/vim/runtime/spell/sv.utf-8.spl";
       hash = "sha256-c+0oRaF2bKIdU4j8dm2SYHSpCnupIjRY/A0Nzsxyq1I=";
-    };
-    "spell/sv.latin1.spl".source = pkgs.fetchurl {
-      url = "ftp://ftp.nluug.nl/pub/vim/runtime/spell/sv.latin1.spl";
-      hash = "sha256-qo/1RIHx7CC0Mv9fezU6P2rJAX2ABrUobZAtiNixl4c=";
     };
   };
 }
