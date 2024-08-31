807815e6c5de1e22dc53c83478c8d38084352c91
LilleAila <olai.solsvik@gmail.com>
2024-08-31 19:14:31 +0200

    snippets: get_visual for delimiters

--- config/plugins/utils/luasnip/snippets/all/autopairs.lua
+++ config/plugins/utils/luasnip/snippets/all/autopairs.lua
@@ -5,7 +5,7 @@ local function delimiterSnip(char1, char2)
   char2 = char2 or char1
   return asnip(
     { trig = "([^%a%dæøåÆØÅ])%" .. char1, wordTrig = false, regTrig = true },
-    { utils.get_cap(1), t(char1), i(1), t(char2) }
+    { utils.get_cap(1), t(char1), d(1, utils.get_visual), t(char2) }
   )
 end
 
