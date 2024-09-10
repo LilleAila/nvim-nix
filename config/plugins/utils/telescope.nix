c109befc0dcd26f42343fff995cdf8b2219b5fd9
LilleAila <olai.solsvik@gmail.com>
2024-09-10 08:05:33 +0200

    telescope: change layout

--- config/plugins/utils/telescope.nix
+++ config/plugins/utils/telescope.nix
@@ -40,6 +40,72 @@
       };
     };
     settings.defaults = {
+      # https://github.com/nvim-telescope/telescope.nvim/issues/848
+      # https://github.com/nvim-telescope/telescope.nvim/blob/master/lua/telescope/themes.lua
+
+      # ivy-like layout
+      layout_strategy = "bottom_pane";
+      layout_config = {
+        height = 0.4;
+      };
+      border = false;
+      sorting_strategy = "ascending";
+
+      # dropdown
+      # results_title = false;
+      # sorting_strategy = "ascending";
+      # layout_strategy = "center";
+      # layout_config = {
+      #   preview_cutoff = 1;
+      #   width = {
+      #     __raw = ''
+      #       function(_, max_columns, _)
+      #         return math.min(max_columns, 80)
+      #       end
+      #     '';
+      #   };
+      #   height = {
+      #     __raw = ''
+      #       function(_, _, max_lines)
+      #         return math.min(max_lines, 20)
+      #       end
+      #     '';
+      #   };
+      # };
+      # border = true;
+      # borderchars = {
+      #   prompt = [
+      #     "─"
+      #     "│"
+      #     " "
+      #     "│"
+      #     "╭"
+      #     "╮"
+      #     "│"
+      #     "│"
+      #   ];
+      #   results = [
+      #     "─"
+      #     "│"
+      #     "─"
+      #     "│"
+      #     "├"
+      #     "┤"
+      #     "╯"
+      #     "╰"
+      #   ];
+      #   preview = [
+      #     "─"
+      #     "│"
+      #     "─"
+      #     "│"
+      #     "╭"
+      #     "╮"
+      #     "╯"
+      #     "╰"
+      #   ];
+      # };
+      #
       mappings = {
         i = {
           "<esc>".__raw = ''
